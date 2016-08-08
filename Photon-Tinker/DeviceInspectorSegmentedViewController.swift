f37da11e1a40d16eaa387f6ba53a1db9eb240946
Ido Kleinman <idokleinman@gmail.com>
2016-08-08 16:11:32 -0700

    start

---
+++ Photon-Tinker/DeviceInspectorSegmentedViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DeviceInspectorSegmentedViewController.swift
+//  Particle
+//
+//  Created by Ido Kleinman on 8/8/16.
+//  Copyright © 2016 spark. All rights reserved.
+//
+
+import Cocoa
+
+class DeviceInspectorSegmentedViewController: MXSegmentedPagerController {
+
+}
